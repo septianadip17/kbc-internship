66cef5415d7745990ec4de278fe4985ed10a776a
iay <septianadip7998@gmail.com>
2024-05-13 08:40:23 +0700

    nav logo link integrated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,11 +51,13 @@ function Navbar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://kbc.or.id/img/general/KBC-Logo_1594107681.png"
-                    alt="KBC Logo"
-                  />
+                  <Link to="/">
+                    <img
+                      className="h-10 w-auto justify-center"
+                      src="https://kbc.or.id/img/general/KBC-Logo_1594107681.png"
+                      alt="KBC Logo"
+                    />
+                  </Link>
                 </div>
                 {/* Navbar Web */}
                 <div className="hidden sm:ml-auto sm:block">
