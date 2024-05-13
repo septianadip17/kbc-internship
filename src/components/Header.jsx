38aaa78e13488738a1dd1010a3acd9d8c950b78d
iay <septianadip7998@gmail.com>
2024-05-13 11:52:47 +0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,22 @@
+import header_background from "../assets/header-home.png";
+import kbcLogo from "../assets/KBC_Logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div
+      className="bg-cover bg-center h-screen flex flex-col justify-center items-center"
+      style={{ backgroundImage: `url(${header_background})` }}
+    >
+      <div className="flex flex-col items-center">
+        <img className="max-w-xl max-h-30 mb-5" src={kbcLogo} alt="KBC Logo" />
+        <h1 className="mb-4 text-4xl font-extrabold text-white md:text-5xl lg:text-6xl text-center uppercase">
+          Welcome To <br />
+          Kingdom Business Community <br />
+          Surabaya
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,14 @@
 import Register from "../components/Register";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import header_background from "../assets/header-home.png";
-import kbcLogo from "../assets/KBC_Logo.png";
+import Header from "../components/Header";
+
 
 const Home = () => {
   return (
     <div className="relative text-center">
       <Navbar />
-      <div
-        className="bg-cover bg-center h-screen flex flex-col justify-center items-center"
-        style={{ backgroundImage: `url(${header_background})` }}
-      >
-        <div className="flex flex-col items-center">
-          <img className="max-w-xl max-h-30 mb-5" src={kbcLogo} alt="KBC Logo" />
-          <h1 className="mb-4 text-4xl font-extrabold text-white md:text-5xl lg:text-6xl text-center uppercase">
-            Welcome To <br />
-            Kingdom Business Community <br />
-            Surabaya
-          </h1>
-        </div>
-      </div>
-
+      <Header />
       <Register />
       <Footer />
     </div>
