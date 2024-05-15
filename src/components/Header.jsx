5a22190f378eb5c65a803b24d8cf77a501546970
iay <septianadip7998@gmail.com>
2024-05-15 16:21:56 +0700

    mengatur logo di head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       style={{ backgroundImage: `url(${header_background})` }}
     >
       <div className="flex flex-col items-center">
-        <img className="max-w-xl max-h-30 mb-5" src={kbcLogo} alt="KBC Logo" />
+        <img className="h-60 mb-10" src={kbcLogo} alt="KBC Logo" />
         <h1 className="mb-4 text-4xl font-extrabold text-white md:text-5xl lg:text-6xl text-center uppercase">
           Welcome To <br />
           Kingdom Business Community <br />
