da1d20796c783cfebb3060581c693bfa8b33bedc
iay <septianadip7998@gmail.com>
2024-05-18 06:36:30 +0700

    margin top login mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ function Navbar() {
                     </DisclosureButton>
                   ))}
                   <Link to="/login">
-                    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-2">
                       Login
                     </button>
                   </Link>
